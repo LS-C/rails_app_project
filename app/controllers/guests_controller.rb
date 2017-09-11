3c12b937d3f8b215dcb22bfab77eadde73409baa
github username <github email address>
2017-09-11 15:46:26 -0400

    db seeded

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,2 +1,52 @@
 class GuestsController < ApplicationController
+  before_action :require_login, only: :show
+
+  def index
+    render :index
+  end
+
+  def show
+    @guest = Guest.find(params[:id])
+  end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.new(guest_params)
+    if @guest.save
+      #check if it's an admin
+      if @guest.admin
+        session[:admin]
+      end
+      session[:guest_id] = @guest.id
+      redirect_to @guest
+      else
+      #re-render the form
+      render :new
+    end
+  end
+
+  def edit
+    @guest = Guest.find(params[:id])
+  end
+
+  def update
+    @guest = Guest.find(params[:id])
+    @guest.update(guest_params)
+  end
+
+  private
+
+  def require_login
+    if not_logged_in
+      redirect_to root_path
+    end
+  end
+
+  def guest_params
+    params.require(:guest).permit(:name)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+suzy = Guest.create(name: "Suzy")
+jil = Guest.create(name: "Jil")
+kevin = Guest.create(name: "Kevin")
+sophie = Guest.create(name: "Sophie")
+justin = Guest.create(name: "Justin")
+
+kimpton = Hotel.create(name: "Kimpton NYC", city_id: 1)
+royalton = Hotel.create(name: "Royalton NYC", city_id: 1)
+sheraton = Hotel.create(name: "Sheraton Shanghai", city_id: 2)
+peninsula = Hotel.create(name: "Peninsula Paris", city_id: 4)
+marriott = Hotel.create(name: "Marriott South Africa", city_id: 5)
+
+nyc = City.create(name: "New York City")
+shanghai = City.create(name: "Shanghai")
+london = City.create(name: "London")
+paris = City.create(name: "Paris")
+sa = City.create(name: "South Africa")
