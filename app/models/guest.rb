fab0cfc8bc0519eb54731466dde26cfa685dd0cf
LS-C <suzy.cho111@gmail.com>
2017-09-14 16:46:41 -0400

    payment_form_working

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -3,10 +3,21 @@ class Guest < ApplicationRecord
   has_many :reservations
   has_many :hotels, through: :reservations
 
+
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates_confirmation_of :password, :message => "Your password does not match."
+
+  # validates :name_on_card, presence: true
+  # validates :cc_type, presence: true
+
+
+  # validates :membership_type, presence: true
+  # validates :credit_card, length: { is: 16 }
+  # validates :cc_exp_date, presence: true
+
   has_secure_password
 
-  # validates :name, uniqueness: true, presence: true
-  # validates_confirmation_of :password, :message => "Your password does not match."
 
 
 
