48e61a0f5c7dd843fecc8af78bf12bce98f6bd92
github username <github email address>
2017-09-15 13:40:09 -0400

    demo ready

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915142117) do
+ActiveRecord::Schema.define(version: 20170915152719) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
