b65e3aa00b89ba83ea2d9deb6fcc68015fbab232
github username <github email address>
2017-11-16 14:02:30 -0500

    almost final version for science fair

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
   helper_method :logged_in?, :current_guest
 
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in to access this section"
-      redirect_to new_guest_path 
+      redirect_to new_guest_path
     end
   end
 
